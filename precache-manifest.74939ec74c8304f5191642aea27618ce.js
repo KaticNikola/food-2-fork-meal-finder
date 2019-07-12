abaf084b2aa4e44de22d271034f17098882b3da6
Katic Nikola <nikolakatic1@gmail.com>
2019-07-12 23:20:57 +0200

    Updates

---
+++ precache-manifest.74939ec74c8304f5191642aea27618ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4011a627eee952e80f4c96f4abee50a3",
+    "url": "/food-2-fork-meal-finder/index.html"
+  },
+  {
+    "revision": "41b869c2aff623449a2a",
+    "url": "/food-2-fork-meal-finder/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ce20973849aeccbe9c34",
+    "url": "/food-2-fork-meal-finder/static/css/main.7d4147c4.chunk.css"
+  },
+  {
+    "revision": "41b869c2aff623449a2a",
+    "url": "/food-2-fork-meal-finder/static/js/2.baaf4887.chunk.js"
+  },
+  {
+    "revision": "ce20973849aeccbe9c34",
+    "url": "/food-2-fork-meal-finder/static/js/main.22fb7f1b.chunk.js"
+  },
+  {
+    "revision": "c579a658d795a2befbef",
+    "url": "/food-2-fork-meal-finder/static/js/runtime~main.e1175765.js"
+  },
+  {
+    "revision": "93e8cb0085975504d91e8198db257278",
+    "url": "/food-2-fork-meal-finder/static/media/defaultBcg.93e8cb00.jpeg"
+  },
+  {
+    "revision": "81c3a452f338e62f2df7f09f7cedf2a7",
+    "url": "/food-2-fork-meal-finder/static/media/homeBcg.81c3a452.jpeg"
+  },
+  {
+    "revision": "cc3fff7e1fb8fcdf7ec881e0a394ee72",
+    "url": "/food-2-fork-meal-finder/static/media/logo.cc3fff7e.svg"
+  }
+]);
